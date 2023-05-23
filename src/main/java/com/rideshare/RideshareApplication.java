1614e27e8c8b3cfcf776054bf99ddbe3f8025b49
taeho <rktlrhrl531@naver.com>
2023-05-23 10:46:35 +0900

    cors 수정

--- src/main/java/com/rideshare/RideshareApplication.java
+++ src/main/java/com/rideshare/RideshareApplication.java
@@ -2,12 +2,32 @@ package com.rideshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RideshareApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RideshareApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:8080")
+						.allowedMethods(HttpMethod.GET.name(),
+								HttpMethod.HEAD.name(),
+								HttpMethod.POST.name(),
+								HttpMethod.PUT.name(),
+								HttpMethod.DELETE.name());
+			}
+		};
 	}
 
 }
