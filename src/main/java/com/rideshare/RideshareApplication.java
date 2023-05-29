d363ed84eb5a7a29fe3c6b84e478f43b4e252aba
taeho <rktlrhrl531@naver.com>
2023-05-29 23:56:03 +0900

    cors 다시한번

--- src/main/java/com/rideshare/RideshareApplication.java
+++ src/main/java/com/rideshare/RideshareApplication.java
@@ -2,10 +2,7 @@ package com.rideshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class RideshareApplication {
@@ -13,7 +10,7 @@ public class RideshareApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RideshareApplication.class, args);
 	}
-
+/*
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
@@ -27,6 +24,6 @@ public class RideshareApplication {
 						.allowCredentials(true);
 			}
 		};
-	}
+	}*/
 
 }
